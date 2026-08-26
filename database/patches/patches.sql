756782d246b3d329cf7df17c6a1b0f03f4e5793e
dibr <daniel.bratov@unicontsoft.com>
2026-08-26 09:41:20 +0300

    patch: Add table sys_Roles and columns in sys_Users

--- database/patches/patches.sql
+++ database/patches/patches.sql
@@ -60,3 +60,52 @@ BEGIN
 	END
 END
 GO
+
+IF	OBJECT_ID('sys_Roles') IS NULL
+BEGIN
+	----------------------------------------------------------------------------------------
+	RAISERROR ('20260824: Add table sys_Roles and columns in sys_Users', 10, 1) WITH NOWAIT
+	----------------------------------------------------------------------------------------
+
+	IF @@TRANCOUNT > 0 ROLLBACK BEGIN TRAN
+	DECLARE		@ERR INT
+	SET			@ERR = 0
+
+	CREATE TABLE sys_Roles (
+				  ID		UNIQUEIDENTIFIER	NOT NULL
+				, Name		NVARCHAR(50)		NOT NULL
+
+				, CD		DATETIME2(3)		NOT NULL
+				, CU		NVARCHAR(255)		NOT NULL
+				, LD		DATETIME2(3)		NOT NULL
+				, LU		NVARCHAR(255)		NOT NULL
+
+				, CONSTRAINT PK_sys_Roles_ID PRIMARY KEY NONCLUSTERED (ID)
+				, CONSTRAINT CL_sys_Roles_ID UNIQUE CLUSTERED (ID)
+				, CONSTRAINT UQ_sys_Roles_Name UNIQUE (Name)
+	)
+	SET			@ERR = @ERR+@@ERROR
+
+	INSERT		dbo.sys_Roles (ID, Name, CD, CU, LD, LU)
+	VALUES		  (NEWID(), 'Admin', GETDATE(), 'system', GETDATE(), 'system')
+				, (NEWID(), 'Organizer', GETDATE(), 'system', GETDATE(), 'system')
+				, (NEWID(), 'Player', GETDATE(), 'system', GETDATE(), 'system')
+	SET			@ERR = @ERR+@@ERROR
+
+	ALTER TABLE sys_Users ADD
+				  Username	NVARCHAR(50)		NOT NULL
+				, Role_ID	UNIQUEIDENTIFIER	NOT NULL
+				, CONSTRAINT FK_sys_Users_sys_Roles_Role_ID
+					FOREIGN KEY (Role_ID)
+					REFERENCES sys_Roles (ID)
+	SET			@ERR = @ERR+@@ERROR
+
+	IF @ERR = 0 BEGIN
+		PRINT 'OK: ' + CONVERT(VARCHAR, @@TRANCOUNT) + ', ' + { fn CURRENT_TIME() }
+		COMMIT
+	END ELSE BEGIN
+		PRINT '!!!!!!!!!!!!! Err: ' + CONVERT(VARCHAR, @ERR) + ' !!!!!!!!!!!!!!!!!!!' + ', ' + CONVERT(VARCHAR, GETDATE(), 121)
+		ROLLBACK
+	END
+END
+GO
